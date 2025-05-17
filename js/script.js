c05d747911a632ccc94a728b5da6896be57202ff
Erica <eriycmartelli@gmail.com>
2025-05-17 13:48:45 -0300

    Primeiro commit do site feito para o trabalho da Uninter

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("horse_button");
+const audio = document.getElementById("horse_sound")
+button.addEventListener("click", function() {
+    audio.play()
+});
+button.addEventListener("click", function() {
+    alert("cavalo")
+});
