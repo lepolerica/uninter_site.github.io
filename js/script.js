d4e5acd1899b0cdf300e2050d7de402492dd780a
lepolerica <eriycmartelli@gmail.com>
2025-05-17 15:11:44 -0300

    Mudanças feitas no HTML, CSS e JS.
    HTML: foram feitas mudanças em textos, visando atender as requisições feitas no PDF entregue pela Uninter, entretanto também na disposição de elementos do site.
    
    CSS: Mudanças para deixar um visual mais elegante, e algumas adições para que o código esteja de maior qualidade.
    
    JS: Função criada para adicionar um texto debaixo do botão "Clique para ver uma mensagem!".

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const button = document.getElementById("horse_button");
-const audio = document.getElementById("horse_sound")
+const audio = document.getElementById("horse_sound");
+const paragrafo = document.createElement("p");
+paragrafo.textContent  = 'Cavalos possuem 4 dedos.';
+
 button.addEventListener("click", function() {
     audio.play()
 });
 button.addEventListener("click", function() {
     alert("cavalo")
+});
+button.addEventListener("click", function() {
+    document.getElementById("paragrafo").appendChild(paragrafo);
 });
